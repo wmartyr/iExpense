feeb8d7fdb758ee492c96f806f3c3809fa1b5731
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-27 15:29:15 +1030

    Completed Project 7 Part 2

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Woodrow Martyr on 25/1/2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
