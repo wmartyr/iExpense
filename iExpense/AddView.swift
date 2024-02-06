f6bb2b89e40cdf5114a1b4436200f7e3a48ab304
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-02-07 06:37:03 +1030

    Complete Day 46 Navigation Wrap Up Challenge 2.

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AddView: View {
     @Environment(\.dismiss) var dismiss
-    @State private var name = ""
+    @State private var name = "Expense Name"
     @State private var type = "Personal"
     @State private var amount = 0.0
     
@@ -20,8 +20,7 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
-                
+                //TextField("Name", text: $name)
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) {
                         Text($0)
@@ -31,7 +30,8 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add New Expense")
+            .navigationTitle($name)
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Save") {
